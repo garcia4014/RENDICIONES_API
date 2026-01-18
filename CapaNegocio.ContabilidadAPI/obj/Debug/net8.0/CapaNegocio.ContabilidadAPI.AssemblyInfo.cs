8cad336562be0d14f378434b5e6ed76ff60b3f0e
Edgar Jean Pierre Garcia Egusquiza <egarcieg@emeal.nttdata.com>
2026-01-18 13:59:46 -0500

    fix: Change test projects to target .NET 8.0

--- CapaNegocio.ContabilidadAPI/obj/Debug/net8.0/CapaNegocio.ContabilidadAPI.AssemblyInfo.cs
+++ CapaNegocio.ContabilidadAPI/obj/Debug/net8.0/CapaNegocio.ContabilidadAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaNegocio.ContabilidadAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e234f7c49cba770fcd2ca7bce42088dcbacfd8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ded4cf0580657ee11a754a2bce3590556c9559f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaNegocio.ContabilidadAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaNegocio.ContabilidadAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
